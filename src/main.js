74dd4e4ed02bb982bd75f9dae3b7b03735403e8d
shengpeixiang <1062024803@qq.com>
2021-04-27 17:50:58 +0800

    delete testEs6

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import router from "./router";
 import store from "./store";
 import {Button,Input,Form,FormItem,Message,Dialog} from "element-ui"
 
-import {q84,q83,q82} from './utils/testes6'
-
 Vue.config.productionTip = false;
 Vue.use(Button);
 Vue.use(Input);
@@ -14,10 +12,6 @@ Vue.use(FormItem);
 Vue.use(Dialog);
 Vue.prototype.$message=Message;
 Vue.component(Message.name,Message);
-
-// console.log(q84(15));
-// q83();
-// q82();
 
 new Vue({
   router,
