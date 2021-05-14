cfe3b05d7bc723bdcbf01049b2151c76f7cda9db
shengpeixiang <1062024803@qq.com>
2021-05-14 17:04:03 +0800

    Start Echarts

--- src/main.js
+++ src/main.js
@@ -4,18 +4,6 @@ import router from "./router";
 import store from "./store";
 import {Button,Input,Form,FormItem,Message,Dialog,Tooltip,Row,Col} from "element-ui"
 
-import Echarts from 'vue-echarts'
-import { use } from 'echarts/core'
-import { CanvasRenderer } from 'echarts/renderers'
-import { BarChart } from 'echarts/charts'
-import { GridComponent,TooltipComponent } from 'echarts/components'
-use([
-  CanvasRenderer,
-  BarChart,
-  GridComponent,
-  TooltipComponent
-]);
-
 Vue.config.productionTip = false;
 Vue.use(Button);
 Vue.use(Input);
@@ -27,10 +15,6 @@ Vue.use(Row);
 Vue.use(Col);
 Vue.prototype.$message=Message;
 Vue.component(Message.name,Message);
-Vue.component("v-chart",Echarts);
-
-
-
 
 require("./mock.js");
 new Vue({
